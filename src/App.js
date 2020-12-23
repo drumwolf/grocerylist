521253b71dd61ad4f3358d0e3d9327b8b5b9b95a
John Lee <drumwolf@Johns-MacBook-Pro-2.local>
2020-12-23 10:22:16 -0800

    mockup of tag list

--- src/App.js
+++ src/App.js
@@ -1,17 +1,46 @@
-import { Box, Input } from "@chakra-ui/react";
+import { Box, Input, List, ListItem, Tag, TagLabel } from "@chakra-ui/react";
 
 function App() {
   return (
     <Box
       as='main'
       m='40px auto'
-      w='800px'
+      w='480px'
     >
       <Input
         bg='white'
         color='black'
         placeholder="Basic usage"
       />
+      <Box
+        borderRadius='8px'
+        bg='blue.400'
+        mt='20px'
+        p='10px'
+      >
+        <List>
+          <ListItem mb='5px'>
+            <Tag size='lg'>
+              <TagLabel>Green</TagLabel>
+            </Tag>
+          </ListItem>
+          <ListItem mb='5px'>
+            <Tag size='lg'>
+              <TagLabel>Green</TagLabel>
+            </Tag>
+          </ListItem>
+          <ListItem mb='5px'>
+            <Tag size='lg'>
+              <TagLabel>Green</TagLabel>
+            </Tag>
+          </ListItem>
+          <ListItem>
+            <Tag size='lg'>
+              <TagLabel>Green</TagLabel>
+            </Tag>
+          </ListItem>
+        </List>
+      </Box>
     </Box>
   );
 }
